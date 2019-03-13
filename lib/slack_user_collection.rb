9f8a198501af95dc7de9023cff2dc18814ca05e5
brentlavelle <brentlavelle@yahoo.com>
2019-03-13 04:51:10 -0500

    a collection that can diff hashmaps

---
+++ lib/slack_user_collection.rb
@@ -0,0 +1,48 @@
+require 'yaml'
+require_relative '../lib/slack_user'
+
+class SlackUserCollection
+  def initialize(yaml)
+    if yaml.size == 0
+      @user_hashmap = {}
+    else
+      @user_hashmap = YAML.load(yaml)
+    end
+  end
+
+  def size()
+    @user_hashmap.size
+  end
+
+  def user(key)
+    @user_hashmap[key]
+  end
+
+  def keys
+    @user_hashmap.keys
+  end
+
+  def diff(newer)
+    results  = []
+    leftover = {}
+    newer.keys.each do |key|
+      leftover[key] = true
+    end
+    @user_hashmap.each_key do |key|
+      this_user = user(key)
+      new_user  = newer.user(key)
+      if new_user.nil?
+        results << "#{this_user.fl_name}: removed"
+      else
+        leftover.delete(key)
+        if not this_user.same?(new_user)
+          results << "#{this_user.fl_name}: #{this_user.compare(new_user)}"
+        end
+      end
+    end
+    leftover.each_key do |key|
+      results << "#{newer.user(key).fl_name}: added"
+    end
+    results
+  end
+end
---
+++ spec/slack_user_collection_spec.rb
@@ -0,0 +1,62 @@
+require 'rspec'
+require 'json'
+require_relative '../lib/slack_user_collection'
+
+one = "
+---
+U2U66J511: !ruby/object:SlackUser
+  user_id: U2U66J511
+  deleted: false
+  first_name: First
+  last_name: Last
+  email: first_last@example.com
+"
+two = "
+---
+U2U66J511: !ruby/object:SlackUser
+  user_id: U2U66J511
+  deleted: false
+  first_name: First
+  last_name: Last
+  email: first_last@example.com
+U2U66J512: !ruby/object:SlackUser
+  user_id: U2U66J512
+  deleted: false
+  first_name: Second
+  last_name: User
+  email: second_user@example.com
+"
+
+describe SlackUserCollection.new('') do
+  it { is_expected.to be_a(SlackUserCollection) }
+  it { expect(subject.size).to be == 0 }
+end
+
+describe SlackUserCollection.new(one) do
+  it { is_expected.to be_a(SlackUserCollection) }
+  it { expect(subject.size).to be == 1 }
+end
+
+describe SlackUserCollection.new(two) do
+  it { is_expected.to be_a(SlackUserCollection) }
+  it { expect(subject.size).to be == 2 }
+end
+
+describe 'diff' do
+    let(:no_users) { SlackUserCollection.new('') }
+    let(:one_user) { SlackUserCollection.new(one) }
+    let(:two_users) { SlackUserCollection.new(two) }
+
+    it { expect(no_users.diff(no_users)).to eq []}
+    it { expect(one_user.diff(one_user)).to eq []}
+    it { expect(two_users.diff(two_users)).to eq []}
+    it 'add to nothing' do
+      expect(no_users.diff(one_user)).to eq ["First Last: added"]
+    end
+    it 'remove to nothing' do
+      expect(one_user.diff(no_users)).to eq ["First Last: removed"]
+    end
+    it 'remove one from two' do
+      expect(two_users.diff(one_user)).to eq ["Second User: removed"]
+    end
+end
